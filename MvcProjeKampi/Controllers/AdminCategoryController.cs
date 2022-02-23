61fd96975505910ed34b650be28ee66db9f4aa5d
burak koca <bur4k111@gmail.com>
2022-02-23 22:28:34 +0300

    23.02.2022 22:30

--- MvcProjeKampi/Controllers/AdminCategoryController.cs
+++ MvcProjeKampi/Controllers/AdminCategoryController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +14,38 @@ namespace MvcProjeKampi.Controllers
     public class AdminCategoryController : Controller
     {
         // GET: AdminCategory
+
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
         public ActionResult Index()
         {
+            var categoryValues = categoryManager.GetCategoryList();
+            return View(categoryValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            CategoryValidator validationRules = new CategoryValidator();
+            ValidationResult result = validationRules.Validate(category);
+            if (result.IsValid)
+            {
+                categoryManager.CategoryAdd(category);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View();
+            }
         }
     }
 }
